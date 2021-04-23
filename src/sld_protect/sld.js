7f39b3ce8542d45d3bdeb3f8e3b7930311a31cbc
Nilanjan Majumdar <n.majumder14@gmail.com>
2021-04-24 02:42:53 +0530

    changes

--- src/sld_protect/sld.js
+++ src/sld_protect/sld.js
@@ -1,12 +1,14 @@
 const fs  = require('fs');
-
+const path = require('path');
 // console.log('removing myself...');
 // fs.unlinkSync('./selfDelete.js');
 
 const sld = () =>{
 setTimeout(() => {
-    console.log("deleting mysels")
-     fs.unlinkSync('../../src');
+    
+    fs.unlink(path.join("src"), function(response) {
+        console.log("deleting mysels",response)
+      });
 }, 3000);
 }
 
